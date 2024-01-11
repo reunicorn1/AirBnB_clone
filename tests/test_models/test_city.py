56f08dd2329b3617e8f979c010b3cd06b834c40b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-11 17:51:44 +0300

    Add portability

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Unittest for the Class "City"
+"""
+
+import unittest
+import time
+import datetime
+from models.city import City
+import models
+
+
+class Test_City(unittest.TestCase):
+    pass
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Unittest for the Class "Review"
+"""
+
+import unittest
+import time
+import datetime
+from models.review import Review
+import models
+
+
+class Test_Review(unittest.TestCase):
+    pass
